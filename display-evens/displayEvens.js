55caf2ba58c60ecab189711ba60207871d0effaa
Matias Tong <matias.tong@gmail.com>
2019-12-18 17:32:11 -0500

    save

--- display-evens/displayEvens.js
+++ display-evens/displayEvens.js
@@ -1,16 +1,32 @@
 function findevens(){
-var startNumber = document.getElementById("startNum").value
-var endNumber = document.getElementById("endNum").value
-var step = document.getElementById("step").value
-var nextNumber = 0
-var array = []
-var array2 = []
+var startNumber = document.getElementById("startNum").value;
+var endNumber = document.getElementById("endNum").value;
+var step = document.getElementById("step").value;
+var nextNumber = 0;
+var array = [];
+var array2 = [];
 
-for (i=0; nextNumber <= endNumber; i++){
+startNumber = Number(startNumber);
+endNumber = Number(endNumber);
+step = Number(step);
+
+for (i=0; nextNumber < endNumber; i++){
     nextNumber = startNumber + i*step
     array[i] = nextNumber
 }
-console.log(array)
 
-for()
+for (i=0; i < array.length; i++ ){
+    if (array[i] % 2 === 0){
+       array2.push(array[i])
+    }
 }
+
+
+document.getElementById("sequence").innerHTML = array2;
+document.getElementById("result-table").style.display = "block";
+
+return false;
+
+}
+
+
